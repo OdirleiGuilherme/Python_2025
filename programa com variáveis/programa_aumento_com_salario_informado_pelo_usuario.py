d54be9640980874721ca0ab23594e7a113951cd9
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2025-11-05 19:18:00 -0300

    Programa pesquisa de aumento salárial, mas com entrada de dados informado pelo usuário. Fazendo os cálculos restantes de valor que ganho de aumento e valor de salário atualizado.

--- programa com variáveis/programa_aumento_com_salario_informado_pelo_usuario.py
+++ programa com variáveis/programa_aumento_com_salario_informado_pelo_usuario.py
@@ -5,5 +5,5 @@ porcentagem = 18
 aumento = (salario * porcentagem)/100
 salario_atualizado = salario+(salario * porcentagem)/100
 
-print(f'Seu salário de R$ {salario:.2f} foi ajustado em {porcentagem}%, aumetando para R$ {salario_atualizado:.2f} '
+print(f'Seu salário de R$ {salario:.2f} foi ajustado em {porcentagem}%, Aumentando para R$ {salario_atualizado:.2f} '
       f'Tendo um aumento em R$ {aumento:.2f}')
