0ddf1b56b1353afc03a2f2c426ac258ee2b7c6bc
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2025-11-17 23:18:51 -0300

    criando um programa onde salário informado pelo usuário, em seguida é feito calculo informando porcentagem e valor do salário atual e atualizado.

--- programa com variáveis/programa_aumento_com_salario_informado_pelo_usuario.py
+++ programa com variáveis/programa_aumento_com_salario_informado_pelo_usuario.py
@@ -1,9 +1,9 @@
 salario = float(input("Digite seu salário para verificar aumento anual: "))
 
-porcentagem = 18
+porcentagem = 10
 
 aumento = (salario * porcentagem)/100
-salario_atualizado = salario+(salario * porcentagem)/100
+salario_atualizado = salario+aumento
 
 print(f'Seu salário de R$ {salario:.2f} foi ajustado em {porcentagem}%, Aumentando para R$ {salario_atualizado:.2f} '
       f'Tendo um aumento em R$ {aumento:.2f}')
