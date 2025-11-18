c6c4a43b7e3b746a2db17e2042f27de92897a71e
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2025-11-17 23:09:52 -0300

    criando um programa com função, a função criada foi criado para texto.

---
+++ Funções/def_txt.py
@@ -0,0 +1,9 @@
+def título(txt):
+    print('=' * 25)
+    print(txt)
+    print('=' * 25)
+
+# Programa Principal
+título('  A lenda de Ruffy Ghanor  ')
+título('  O Garoto Cabra  ')
+título('  Editora Dark Books  ')
