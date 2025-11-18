cf557252cda945c8a87fa69f2d7f686afab5bf97
Odirlei Guilherme <odirlei.guilherme11@gmail.com>
2025-11-17 23:13:46 -0300

    criando uma função, de subtração simples.

---
+++ Funções/funcao_subtracao.py
@@ -0,0 +1,10 @@
+def subtracao(y, x):
+    print(f'Y = {y} e X = {x}')
+    s = y - x
+    print(f'A subtração entre Y - X = {s}')
+
+
+# Programa Principal
+subtracao(12, 5)
+subtracao(x=9, y=19)
+subtracao(y=30,x=4)
